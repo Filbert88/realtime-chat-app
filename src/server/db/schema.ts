43ffb4a62e623da433f92dc5073609828584110c
Filbert <92862064+Filbert88@users.noreply.github.com>
2024-07-09 19:11:33 +0700

    feat: update schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -20,67 +20,111 @@ import {
 export const createTable = pgTableCreator((name) => `rtc_${name}`);
 
 export const users = createTable("user", {
-  id: uuid("id").notNull().primaryKey().$defaultFn(() => sql`uuid_generate_v4()`),
+  id: uuid("id")
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => sql`uuid_generate_v4()`),
   email: varchar("email", { length: 255 }).notNull().unique(),
   password: text("password").notNull(),
+  phone: varchar("phone", { length: 255 }).notNull().unique(),
   name: varchar("name", { length: 255 }).notNull(),
-  appID: varchar("app_id", { length: 255 }).notNull().unique(),
+  appID: varchar("app_id", { length: 255 }).unique(),
   image: varchar("image_url", { length: 255 }),
   emailVerified: timestamp("email_verified"),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
-  updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`).$onUpdateFn(() => sql`CURRENT_TIMESTAMP`),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .$onUpdateFn(() => sql`CURRENT_TIMESTAMP`),
 });
 
 export const accounts = createTable("account", {
-  userId: uuid("user_id").notNull().references(() => users.id),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id),
   type: varchar("type", { length: 255 }).notNull(),
   provider: varchar("provider", { length: 255 }).notNull(),
   providerAccountId: varchar("provider_account_id", { length: 255 }).notNull(),
-  refresh_token: varchar("refresh_token", { length: 255 }), 
-  access_token: varchar("access_token", { length: 255 }), 
-  expires_at: integer("expires_at"), 
-  token_type: varchar("token_type", { length: 255 }), 
-  scope: varchar("scope", { length: 255 }), 
-  id_token: varchar("id_token", { length: 255 }), 
-  session_state: varchar("session_state", { length: 255 }), 
+  refresh_token: varchar("refresh_token", { length: 255 }),
+  access_token: varchar("access_token", { length: 255 }),
+  expires_at: integer("expires_at"),
+  token_type: varchar("token_type", { length: 255 }),
+  scope: varchar("scope", { length: 255 }),
+  id_token: varchar("id_token", { length: 255 }),
+  session_state: varchar("session_state", { length: 255 }),
 });
 
 export const messages = createTable("msg", {
-  id: integer("id").notNull().primaryKey().$defaultFn(() => sql`nextval('msg_id_seq'::regclass)`),
+  id: integer("id")
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => sql`nextval('msg_id_seq'::regclass)`),
   content: text("content").notNull(),
-  senderId: uuid("sender_id").notNull().references(() => users.id),
-  receiverId: uuid("receiver_id").notNull().references(() => users.id),
+  senderId: uuid("sender_id")
+    .notNull()
+    .references(() => users.id),
+  receiverId: uuid("receiver_id")
+    .notNull()
+    .references(() => users.id),
   conversationId: uuid("conversation_id").notNull(),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
-  updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`).$onUpdateFn(() => sql`CURRENT_TIMESTAMP`),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .$onUpdateFn(() => sql`CURRENT_TIMESTAMP`),
 });
 
 export const conversations = createTable("cvs", {
-  id: uuid("id").notNull().primaryKey().$defaultFn(() => sql`uuid_generate_v4()`),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
-  updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`).$onUpdateFn(() => sql`CURRENT_TIMESTAMP`),
+  id: uuid("id")
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => sql`uuid_generate_v4()`),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .$onUpdateFn(() => sql`CURRENT_TIMESTAMP`),
 });
 
-export const participants = createTable("ptp", {
-  conversationId: uuid("conversation_id").notNull().references(() => conversations.id),
-  userId: uuid("user_id").notNull().references(() => users.id),
-}, (table) => ({
-  pk: primaryKey(table.conversationId, table.userId),
-}));
+export const participants = createTable(
+  "ptp",
+  {
+    conversationId: uuid("conversation_id")
+      .notNull()
+      .references(() => conversations.id),
+    userId: uuid("user_id")
+      .notNull()
+      .references(() => users.id),
+  },
+  (table) => ({
+    pk: primaryKey(table.conversationId, table.userId),
+  }),
+);
 
 export const sessions = createTable("ssion", {
-  sessionToken: varchar("session_token", { length: 255 }).notNull().primaryKey(),
-  userId: uuid("user_id").notNull().references(() => users.id),
+  sessionToken: varchar("session_token", { length: 255 })
+    .notNull()
+    .primaryKey(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id),
   expires: timestamp("expires", { mode: "date" }).notNull(),
 });
 
-export const verificationTokens = createTable("vrf_t", {
-  identifier: varchar("identifier", { length: 255 }).notNull(),
-  token: varchar("token", { length: 255 }).notNull(),
-  expires: timestamp("expires", { mode: "date" }).notNull(),
-}, (table) => ({
-  pk: primaryKey(table.identifier, table.token),
-}));
+export const verificationTokens = createTable(
+  "vrf_t",
+  {
+    identifier: varchar("identifier", { length: 255 }).notNull(),
+    token: varchar("token", { length: 255 }).notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+  },
+  (table) => ({
+    pk: primaryKey(table.identifier, table.token),
+  }),
+);
 
 // Relations
 export const usersRelations = relations(users, ({ many }) => ({
@@ -107,6 +151,9 @@ export const sessionsRelations = relations(sessions, ({ one }) => ({
   user: one(users),
 }));
 
-export const verificationTokensRelations = relations(verificationTokens, ({ one }) => ({
-  user: one(users),
-}));
+export const verificationTokensRelations = relations(
+  verificationTokens,
+  ({ one }) => ({
+    user: one(users),
+  }),
+);
